84efadbf2a4caf5bb498dfab18493885400d8352
Brett Zamir <brettz9@yahoo.com>
2012-08-19 12:50:25 +0800

    allowing script tag standard method and data tags

--- entities.js
+++ entities.js
@@ -16,55 +16,69 @@
                 node
         );
     }
+    function setText (lookup) {
+        if (!map[lookup]) {
+            alert(
+                'You must define an entity "' + lookup +
+                '" within <meta> tags and/or define and add to a "JSEntityMap" JavaScript object variable.'
+            );
+            return;
+        }
+        text = map[lookup];
+    }
+    
+    var text, lookup, entity,
+        d = document, 
+        entityReferenceName = 'entity', // could change to "data" for standard approach
+        attPrefix = entityReferenceName === 'data' ? 'data-' : '',
+        map = JSEntityMap ? JSEntityMap : {},
+        // Might want these next two configurable:
+        metaHeadOnly = false, // True would be a little faster, 
+                                // but wouldn't allow users to define their own 
+                                // (e.g., within a comment or wiki system)
+        entities,
+        metas = (metaHeadOnly ? d.head : d).getElementsByTagName('meta');
+
+    // Requires that meta tags be placed above this script (added here 
+    //  so document.write method e() doesn't need to wait for page domcontentloaded)
+    Array.prototype.forEach.call(metas, function (meta) { // Shortcut Array.forEach() not currently supported in Chrome
+        var pair;
+        if (    meta.hasAttribute(attPrefix+'content') && 
+                meta.hasAttribute(attPrefix+'name') && 
+                meta.getAttribute(attPrefix+'name') === 'entity'
+            ) {
+            pair = meta.getAttribute(attPrefix+'content').split('=', 2);
+            map[pair[0]] = pair[1];
+        }
+    });
     
     // EVENT ATTACHMENT
-    this.addEventListener('load', function () {
-        var lookup, entity, text,
-            d = document, 
-            map = JSEntityMap ? JSEntityMap : {},
-            // Might want these next two configurable:
-            metaHeadOnly = false, // True would be a little faster, 
-                                    // but wouldn't allow users to define their own 
-                                    // (e.g., within a comment or wiki system)
-            entityReferenceName = 'entity',
-            entities = d.getElementsByTagName(entityReferenceName),
-            metas = (metaHeadOnly ? d.head : d).getElementsByTagName('meta');
+    this.addEventListener('DOMContentLoaded', function () {
         
-        Array.prototype.forEach.call(metas, function (meta) { // Shortcut Array.forEach() not currently supported in Chrome
-            var pair;
-            if (    meta.hasAttribute('content') && 
-                    meta.hasAttribute('name') && 
-                    meta.getAttribute('name') === 'entity'
-                ) {
-                pair = meta.getAttribute('content').split('=', 2);
-                map[pair[0]] = pair[1];
-            }
-        });
-        
+        entities = d.getElementsByTagName(entityReferenceName);
         while (entities.length) {
             entity = entities[0];
             // One might add other conditions here to transform to different kinds of elements, etc. or
             //   allow a callback or two to check and define text or replacement node
             // The first two conditions are not very helpful since possible as HTML entities
-            if (entity.hasAttribute('dec')) {
-                text = fixedFromCharCode(parseInt(entity.getAttribute('dec'), 10));
+            if (entity.hasAttribute(attPrefix+'dec')) {
+                text = fixedFromCharCode(parseInt(entity.getAttribute(attPrefix+'dec'), 10));
             }
-            else if (entity.hasAttribute('hex')) {
-                text = fixedFromCharCode(parseInt(entity.getAttribute('hex'), 16));
+            else if (entity.hasAttribute(attPrefix+'hex')) {
+                text = fixedFromCharCode(parseInt(entity.getAttribute(attPrefix+'hex'), 16));
             }
             else {
-                lookup = entity.hasAttribute('name') ? entity.getAttribute('name') : entity.textContent;
-                
-                if (!map[lookup]) {
-                    alert(
-                        'You must define an entity "' + lookup +
-                        '" within <meta> tags and/or define and add to a "JSEntityMap" JavaScript object variable.'
-                    );
-                    break;
-                }
-                text = map[lookup];
+                lookup = entity.hasAttribute(attPrefix+'name') ? entity.getAttribute(attPrefix+'name') : entity.textContent;
+                setText(lookup);
             }
             replaceNodeWithText(entity, text);
         };
     });
+    
+    // EXPORTS
+    // Comment out to avoid globals
+    this.e = function (lookup) {
+        setText(lookup);
+        document.write(text);
+    };
 }());

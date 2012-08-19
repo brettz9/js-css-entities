597685e98602dd66d993ed6f70c20fdeb84d7b25
Brett Zamir <brettz9@yahoo.com>
2012-08-19 16:04:30 +0800

    HTML5 DOCTYPE, IE<9 support and external HTML file with meta entities support

--- entities.js
+++ entities.js
@@ -50,42 +50,13 @@
                 'You must define an entity "' + lookup +
                 '" within <meta> tags and/or define and add to a "JSEntityMap" JavaScript object variable.'
             );
-            return;
+            return false;
         }
         text = map[lookup];
+        return true;
     }
     
-    var text, lookup, entity,
-        d = document, 
-        scripts = document.getElementsByTagName('script'), // Doesn't need to be onload as only getting this script
-        currentScriptURL = scripts[scripts.length-1].src,
-        globalFunction = getURLQueryParam('global'),
-        element = getURLQueryParam('element'),
-        entityReferenceName =  element || 'entity', // could change to "data" for standard approach
-        attPrefix = entityReferenceName === 'data' ? 'data-' : '',
-        map = JSEntityMap ? JSEntityMap : {},
-        // Might want these next two configurable:
-        metaHeadOnly = false, // True would be a little faster, 
-                                // but wouldn't allow users to define their own 
-                                // (e.g., within a comment or wiki system)
-        entities,
-        metas = (metaHeadOnly ? d.head : d).getElementsByTagName('meta');
-
-    // Requires that meta tags be placed above this script (added here 
-    //  so document.write method e() doesn't need to wait for page domcontentloaded)
-    Array.prototype.forEach.call(metas, function (meta) { // Shortcut Array.forEach() not currently supported in Chrome
-        var pair;
-        if (    meta.hasAttribute('content') && 
-                meta.hasAttribute('name') && 
-                meta.getAttribute('name') === 'entity'
-            ) {
-            pair = meta.getAttribute('content').split('=', 2);
-            map[pair[0]] = pair[1];
-        }
-    });
-    
-    // EVENT ATTACHMENT
-    this.addEventListener('DOMContentLoaded', function () {
+    function replaceEntityText () {
         
         entities = d.getElementsByTagName(entityReferenceName);
         while (entities.length) {
@@ -100,15 +71,128 @@
                 text = fixedFromCharCode(parseInt(entity.getAttribute(attPrefix+'hex'), 16));
             }
             else {
-                lookup = entity.hasAttribute(attPrefix+'ent') ? entity.getAttribute(attPrefix+'ent') : 
-                            entityReferenceName === 'data' ? false : entity.textContent;
+                lookup = entity.hasAttribute(attPrefix + 'ent') ? entity.getAttribute(attPrefix + 'ent') : 
+                            entityReferenceName === 'data' ? false : entity.textContent || entity.innerText;
                 if (lookup === false) { // Disallow plain <data> elements as could be used for other purposes
                     break;
                 }
-                setText(lookup);
+                if (!setText(lookup)) {
+                    break;
+                }
             }
             replaceNodeWithText(entity, text);
-        };
+        }
+    }
+
+    function addMetasToMap (metas) {
+        for (var i=0, metalen = metas.length; i < metalen; i++) {
+            var pair, meta = metas[i];
+            if (    meta.hasAttribute('content') && 
+                    meta.hasAttribute('name') && 
+                    meta.getAttribute('name') === 'entity'
+                ) {
+                pair = meta.getAttribute('content').split('=', 2);
+                map[pair[0]] = pair[1];
+            }
+        }
+    }
+    
+    function addListener (obj, type, listener) {
+        if (obj.addEventListener) {
+            obj.addEventListener(type, listener, false);
+        }
+        else {
+            if (type === 'DOMContentLoaded') { // Older listener support in IE coincides with non-support of DOMContentLoaded
+                type = 'load';
+            }
+            obj.attachEvent('on' + type, listener);
+        }
+    }
+
+    function addStylesheetRules (decls) {
+        var style = document.createElement('style');
+        document.getElementsByTagName('head')[0].appendChild(style);
+        if (!window.createPopup) { /* For Safari */
+           style.appendChild(document.createTextNode(''));
+        }
+        var s = document.styleSheets[document.styleSheets.length - 1];
+        for (var i=0, dl = decls.length; i < dl; i++) {
+            var j = 1, decl = decls[i], selector = decl[0], rulesStr = '';
+            if (Object.prototype.toString.call(decl[1][0]) === '[object Array]') {
+                decl = decl[1];
+                j = 0;
+            }
+            for (var rl=decl.length; j < rl; j++) {
+                var rule = decl[j];
+                rulesStr += rule[0] + ':' + rule[1] + (rule[2] ? ' !important' : '') + ';\n';
+            }
+     
+            if (s.insertRule) {
+                s.insertRule(selector + '{' + rulesStr + '}', s.cssRules.length);
+            }
+            else { /* IE */
+                s.addRule(selector, rulesStr, -1);
+            }
+        }
+    }
+    function entityIframeLoad (e) {;
+        ct++;
+        var iwin = (e.target || e.srcElement).contentWindow,
+            idoc = iwin.document,
+            imetas = (metaHeadOnly ? idoc.head : idoc).getElementsByTagName('meta');
+        addMetasToMap(imetas);
+    }
+    
+    var text, lookup, entity,
+        d = document, 
+        ct = 0,
+        ifrs = [],
+        scripts = document.getElementsByTagName('script'), // Doesn't need to be onload as only getting this script
+        currentScriptURL = scripts[scripts.length-1].src,
+        globalFunction = getURLQueryParam('global'),
+        element = getURLQueryParam('element'),
+        entityReferenceName =  element || 'entity', // could change to "data" for standard approach
+        attPrefix = entityReferenceName === 'data' ? 'data-' : '',
+        map = JSEntityMap ? JSEntityMap : {},
+        // Might want these next two configurable:
+        metaHeadOnly = false, // True would be a little faster, 
+                                // but wouldn't allow users to define their own 
+                                // (e.g., within a comment or wiki system)
+        entities,
+        metas = (metaHeadOnly ? d.head : d).getElementsByTagName('meta');
+        
+    // IE will probably need this for styling unless using a standard element like <div> or <span>
+    document.createElement(entityReferenceName);
+
+    // Avoid need for defining styles to hide the meta iframes
+    addStylesheetRules([['iframe[data-meta]', ['display', 'none']]]);
+    
+    // Requires that meta tags be placed above this script (added here 
+    //  so document.write method e() doesn't need to wait for page domcontentloaded,
+    //  though called again by necessity if data-meta iframes detected)
+    addMetasToMap(metas);
+    
+    // EVENT ATTACHMENT    
+    addListener(this, 'DOMContentLoaded', function () {
+        var iframes = d.getElementsByTagName('iframe');
+        for (var i = 0, il = iframes.length; i < il; i++) {
+            var iframe = iframes[i];
+            if (iframe.hasAttribute('data-meta')) {
+                ifrs.push(iframe);
+            }
+        }
+        
+        if (!ifrs.length) { // There are no meta iframes, so no need to wait
+            replaceEntityText();
+        }
+        else {
+            var intrvl = setInterval(function () {
+                if (ct === ifrs.length) {
+                    replaceEntityText();
+                    clearInterval(intrvl);
+                }
+            }, 100);
+        }
     });
     
     // EXPORTS
@@ -118,4 +202,5 @@
             document.write(text);
         };
     }
+    this.entityIframeLoad = entityIframeLoad; // Needed by IE which needs inline onload on iframes
 }());
